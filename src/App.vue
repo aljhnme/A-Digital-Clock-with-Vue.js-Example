8065354343889001cb14d173af2248252a81056a
M.Shaker Khazal <52810715+aljhnme@users.noreply.github.com>
2023-03-24 16:31:20 -0700

    clock

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <IdClock />
+</template>
+
+<script>
+import IdClock from './components/IdClock.vue';
+import './assets/css/style.css';
+
+export default {
+  name: 'App',
+  components: {
+    IdClock
+  }
+}
+</script>
---
+++ src/components/IdClock.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="clock">
+        <p class="date">{{ date }}</p>
+        <p class="time">{{ time }}</p>
+    </div>
+</template>
+<script type="text/javascript">
+export default {
+
+    name: 'IdClock',
+
+    data() {
+        return {
+            date: "0000-00-00",
+            time: "00:00:00",
+
+        }
+    },
+    methods: {
+        ShowDTnow: function() {
+
+            const dThisNow = this;
+
+            setInterval(function() {
+
+                var tdATEN = new Date();
+                dThisNow.date = tdATEN.getFullYear() + "-" + (tdATEN.getMonth() + 1) + "-" + tdATEN.getDate();
+
+                dThisNow.time = tdATEN.getHours() + ":" + tdATEN.getMinutes() + ":" + tdATEN.getSeconds();
+
+            }, 1000)
+        }
+    },
+    mounted() {
+        this.ShowDTnow();
+    }
+}
+</script>
